94b5dec91fa81dc3b1d566392945d1a48b09f0c2
altintahiraj <altintahiraj357@gmail.com>
2026-02-27 16:48:21 +0100

    exercise 8

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,4 +1,2 @@
--- Write a SQL query to find the number of records in the first_table where the id is 89.
-SELECT COUNT(*) 
-FROM first_table 
-WHERE id = 89;
+-- Write a SQL query to find the top score in the second_table and display the score and the name of the person who has that score.
+SELECT score, name FROM second_table ORDER BY score DESC;
--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
--- script to count the number of rows in the table first_table where name is "Best School"
-SELECT COUNT(*) FROM first_table WHERE name = "Best School";
+-- Write a SQL query to find the number of records in the first_table where the id is 89.
+SELECT COUNT(*) FROM first_table WHERE id = 89;
